9b725d00370bf7a8782d48affea11a4f2bf4c7a4
AzaleeX <azaleemcpe@gmail.com>
2024-08-30 20:04:19 +0200

    update: star size

--- src/components/utils/SparklesText.tsx
+++ src/components/utils/SparklesText.tsx
@@ -71,11 +71,11 @@ const SparklesText: React.FC<SparklesTextProps> = ({
 
     useEffect(() => {
         const generateStar = (): Sparkle => {
-            const starX = `${Math.random() * 100}%`;
+            const starX = `${Math.random() * 95}%`;
             const starY = `${Math.random() * 100}%`;
             const color = Math.random() > 0.5 ? colors.first : colors.second;
             const delay = Math.random() * 2;
-            const scale = Math.random() * 1 + 0.3;
+            const scale = Math.random() + 0.3;
             const lifespan = Math.random() * 10 + 5;
             const id = `${starX}-${starY}-${Date.now()}`;
             return { id, x: starX, y: starY, color, delay, scale, lifespan };
